a72ff177485f97e9fe484005fcd811f9d33062db
JonQ97 <jonq97@gmail.com>
2017-11-28 04:23:01 -0800

    Ver 19

---
+++ controllers/database.js
@@ -0,0 +1,43 @@
+var mongodb = require('mongodb');
+//var mongoDBURI = process.env.MONGODB_URI || 'mongodb://project2TestUser:project2Test@ds064198.mlab.com:64198/project-2-orders';
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://project2TestUser:project2Test@ds255715.mlab.com:55715/heroku_nkdzppr7';
+
+/** getAllRoutes controller logic that current does model logic too -connects to Mongo database and
+ * queries the Routes collection to retrieve all the routes and build the output usig the
+ * ejs template mongodb.ejs found in views directory
+ * @param request
+ * @param response
+ */
+
+module.exports.getAllOrders =  function (request, response) {
+
+    mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
+        if(err) throw err;
+
+        //get collection of orders
+        var Orders = db.collection('ORDERS');
+
+        //FIRST showing you one way of making request for ALL ORDERS and cycle through with a forEach loop on returned Cursor
+        //   this request and loop  is to display content in the  console log
+        var c = Orders.find({});
+
+        c.forEach(
+            function(myDoc) {
+                console.log( "name: " + myDoc.name );  //just logging the output to the console
+            }
+        );
+
+        //SECOND -show another way to make request for ALL orders and simply collect the documents as an
+        //   array called docs that you  forward to the getAllOrders.ejs view for use there
+        Orders.find().toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('getAllOrders', {results: docs});
+
+        });
+        //close connection when your app is terminating.
+        db.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
+};//end function
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,16 @@ var router = express.Router();
 var mongodb = require('mongodb');
 module.exports = router;
 
+
+
+
+
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Hello World!' });
 });
+
 
 //########################################
 //to process data sent in on request need body-parser module
@@ -20,23 +25,42 @@ router.use(bodyParser.json()); // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
 //#########################################
 
+
+
+
+router.post('/readNameAndRespond', function(req, res, next) {
+
+
+    //expecting data variable called name --retrieve value using body-parser
+    var body = JSON.stringify(req.body);  //if wanted entire body as JSON
+    var params = JSON.stringify(req.params);//if wanted parameters
+    //expecting data variable called name --retrieve value using body-parser
+
+    var value_name = req.body.name;  //retrieve the data associated with name
+
+    //var value_name = req.params.name;  //retrieve the data associated with name
+
+    //res.render('readNameAndRespond', {outputName: req.params.name })
+
+    res.send("hello " + value_name);
+});
+
+
+
 //LOAD the various controllers
 //var controllerMain = require('../controllers/main');   //this will load the main controller file
 
-//var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Routes collection
+var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Routes collection
 var controllerstoreData = require('../controllers/storeData');
 //MAY HAVE OTHER CODE in index.js
+
 
 //CODE to route /getAllRoutes to appropriate  Controller function
 //**************************************************************************
 //***** mongodb get all of the Routes in Routes collection w
 //      and Render information iwith an ejs view
 
-router.get('/storeData', controllerstoreData.storeData);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-
+router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
 
 //neither app.post nor router.post worked for me to call "storeData" so I took a different route
 //app.post('/storeData', controllerstoreData.storeData);
@@ -121,8 +145,3 @@ router.post('/readOrderAndRespond', function(req, res, next) {
 */
 
 //router.post('/insert')
-
-
-
-
-
